36d75f3e17682b55673075af72fce3bc7472348e
Richard <lasso_1995@hotmail.com>
2021-09-29 19:26:35 -0500

    Cart Widget navbar fixed

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,15 +3,19 @@ import { UseCart } from '../context/CartContext'
 
 export const CartWidget = () => {
 
-  const { getQuantity } = UseCart();
+  const { getQuantity, cart } = UseCart();
 
 
   return (
     <>
-      <div className="cart-widget">
-        <img className="cart-widget__icon" src={Bag} alt="bag icon" />
-        <div className="cart-widget__number">{getQuantity()}</div>
-      </div>
+      {
+        cart.length !== 0 ?
+          <div className="cart-widget">
+            <img className="cart-widget__icon" src={Bag} alt="bag icon" />
+            <div className="cart-widget__number">{getQuantity()}</div>
+          </div>
+          : null
+      }
     </>
   )
 }
